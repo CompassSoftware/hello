06889df1524aa94a3ee1df4951d434c79d210de1
Jay Fenwick <fenwickjb@appstate.edu>
2016-01-25 14:22:22 -0500

    Update HelloTest.java

--- src/HelloTest.java
+++ src/HelloTest.java
@@ -11,13 +11,17 @@ public class HelloTest {
 
 	@Test
 	public void test() {
+		// Redirect System.out to a sring
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(baos));
 		
+		// Prep done, call function of interest
 		Hello.main(null);
 		
+		// Restore the stdout stream
 		System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
 		
+		// Check for desired result
 		String s = baos.toString();
 		if (! "Hello, world.\n".equals(s))
 			fail("Not yet implemented");
