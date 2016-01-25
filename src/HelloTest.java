a2787dfab96c650e2c3742cf52bede0c34d8e9e4
fenwickjb <fenwickjb@ip-152010131207.cs.appstate.edu>
2016-01-25 14:20:08 -0500

    hello world unit test working

--- src/HelloTest.java
+++ src/HelloTest.java
@@ -1,12 +1,26 @@
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 import org.junit.Test;
 
 public class HelloTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(baos));
+		
+		Hello.main(null);
+		
+		System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+		
+		String s = baos.toString();
+		if (! "Hello, world.\n".equals(s))
+			fail("Not yet implemented");
 	}
 
 }
