4f98f147d0ee1ffe8b828548296e32f0e8f8a859
fenwickjb <fenwickjb@ip-152010131207.cs.appstate.edu>
2016-01-25 14:47:59 -0500

    Custom personalization allowed.

--- src/HelloTest.java
+++ src/HelloTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 public class HelloTest {
 
 	@Test
-	public void test() {
+	public void testMain1() {
 		// Redirect System.out to a sring
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(baos));
@@ -24,7 +24,65 @@ public class HelloTest {
 		// Check for desired result
 		String s = baos.toString();
 		if (! "Hello, Jay.\n".equals(s))
-			fail("Not yet implemented");
+			fail("Unexpected output:\n"+s+"\n");
+	}
+	
+	@Test
+	public void testMain2() {
+		// Redirect System.out to a sring
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(baos));
+		
+		// Prep done, call function of interest
+		String[] sarray = new String[1];
+		sarray[0] = new String("Jayne");
+		Hello.main(sarray);
+		
+		// Restore the stdout stream
+		System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+		
+		// Check for desired result
+		String s = baos.toString();
+		if (! "Hello, Jayne.\n".equals(s))
+			fail("Unexpected output:\n"+s+"\n");
+	}
+
+	@Test
+	public void testDefaultMessage() {
+		// Redirect System.out to a sring
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(baos));
+		
+		// Prep done, call function of interest
+		Hello.defaultMessage();
+		
+		// Restore the stdout stream
+		System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+		
+		// Check for desired result
+		String s = baos.toString();
+		if (! "Hello, Jay.\n".equals(s))
+			fail("Unexpected output:\n"+s+"\n");
+
+	}
+	
+	@Test
+	public void testPersonalizedMessage() {
+		// Redirect System.out to a sring
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(baos));
+		
+		// Prep done, call function of interest
+		Hello.personalizedMessage("John");
+		
+		// Restore the stdout stream
+		System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+		
+		// Check for desired result
+		String s = baos.toString();
+		if (! "Hello, John.\n".equals(s))
+			fail("Unexpected output:\n"+s+"\n");
+
 	}
 
 }
